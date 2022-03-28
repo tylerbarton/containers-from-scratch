60eb55bec23bede7c47a6ba732b8542c907b42a3
tyler <tylervbarton@gmail.com>
2022-03-27 20:40:07 -0500

    Add different container type definitions

--- src/container.cpp
+++ src/container.cpp
@@ -3,7 +3,29 @@
 //
 #define _GNE_SOURCE
 #include <cstdio>
+#include <sched.h>
+#include <sys/wait.h>
+#include <iostream>
+
+// Container Types
+const int DEFAULT       = SIGCHLD | CLONE_NEWNS;
+const int PID_Flags     = SIGCHLD | CLONE_NEWPID;
+const int MNT_Flags     = SIGCHLD | CLONE_NEWNS;
+const int NET_Flags     = SIGCHLD | CLONE_NEWNET;
+const int USER_Flags    = SIGCHLD | CLONE_NEWUSER;
+const int UTS_Flags     = SIGCHLD | CLONE_NEWUTS;
+const int IPC_Flags     = SIGCHLD | CLONE_NEWIPC;
+
+// Change this to modify the container type
+static int clone_flags = DEFAULT;
+
+typedef struct clone_args{
+    char **argv;
+} cloneArgs;
 
 int main(int argc, char* argv[]){
-    printf("Hello World");
+    struct clone_args args;
+    args.argv = &argv[1];
+
+    std::cout << args.argv;
 }
